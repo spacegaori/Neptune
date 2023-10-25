1f28df6cf25c5224d7bfdbdd919cd773a1c663cc
Soomin Choi <spacegaori@gmail.com>
2023-10-24 17:59:11 -0700

    clean up ignored files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/spacegaori/dev/neptune
-# Build directory: /Users/spacegaori/dev/neptune/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-include("/Users/spacegaori/dev/neptune/build/matrix_test[1]_include.cmake")
-subdirs("_deps/googletest-build")
