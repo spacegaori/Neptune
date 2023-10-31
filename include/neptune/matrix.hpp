b47f78bc516f1252fdf3b193c91d7373ac55ea7d
Soomin Choi <spacegaori@gmail.com>
2023-10-30 19:55:41 -0700

    added operator overloading and moved all declarations within the header

--- include/neptune/matrix.hpp
+++ include/neptune/matrix.hpp
@@ -7,6 +7,8 @@
 #include <cmath>
 #include <concepts>
 #include <cstddef>
+#include <format>
+#include <functional>
 #include <iostream>
 #include <iterator>
 #include <random>
@@ -14,86 +16,113 @@
 #include <type_traits>
 #include <vector>
 
-float rand_float();
-float sigmoidf(float x);
+namespace Neptune {
 
 using Vector = std::vector<float>;
 using Index = Vector::size_type;
-
 class Matrix {
 private:
-    Index m_rows {};
-    Index m_cols {};
-    Vector m_elements {};
+    Index rows_ {};
+    Index cols_ {};
+    Vector elements_ {};
     
 public:
-    Matrix(Index rows = 0, Index cols = 0) : m_rows{ rows }, m_cols{ cols }, m_elements{ Vector(rows*cols, 0.0f) } {}
-    Matrix(Index rows, Index cols, Vector elements) : m_rows{ rows }, m_cols{ cols }, m_elements{ elements } {}
-
-    void alloc(Vector es)
+    Matrix(Index rows = 0, Index cols = 0, float f = 0.0f) : rows_{ rows }, cols_{ cols }, elements_{ Vector(rows*cols, f) } {}
+    Matrix(Index rows, Index cols, Vector elements) : rows_{ rows }, cols_{ cols }, elements_{ elements }
     {
-        m_elements = es;
+        assert(rows*cols == std::size(elements));
     }
 
-    auto at(Index i)
+    float& operator[] (Index index)
     {
-        return m_elements.at(i);
+        assert(index >= 0 && index < std::size(elements_));
+        return elements_.at(index);
+    }
+
+    friend std::ostream& operator<<(std::ostream& out, const Matrix& matrix)
+    {
+        assert(0 < std::size(matrix.elements_));
+        out << "[\n";
+        for (Index i{ 0 }; i < matrix.rows_; ++i) {
+            for (Index j{ 0 }; j < matrix.cols_; ++j) {
+                out << "  " << matrix.elements_.at(i*matrix.cols_+j) << "  ";
+            }
+            out << '\n' << '\n';
+        }
+        out << "]\n";
+
+        return out;
+    }
+
+    bool operator! () const {
+        return (rows_*cols_ > 0);
+    }
+
+    friend Matrix operator+(const Matrix& m, const Matrix& n) {
+        assert(m.rows_ == n.rows_);
+        assert(m.cols_ == n.cols_);
+        Vector u = m.elements_;
+        Vector v = n.elements_;
+        std::transform(u.cbegin(), u.cend(), v.cbegin(), u.begin(), std::plus<float>());
+
+        return Matrix{ m.rows_, m.cols_, u };
+    }
+
+    friend Matrix operator+(const Matrix& m, float value) {
+        Vector v = m.elements_;
+        std::transform(v.cbegin(), v.cend(),v.begin(), [value](float f){return f + value;});
+
+        return Matrix{ m.rows_, m.cols_, v };
+    }
+
+    friend Matrix operator-(const Matrix& m, const Matrix& n) {
+        assert(m.rows_ == n.rows_);
+        assert(m.cols_ == n.cols_);
+        Vector u = m.elements_;
+        Vector v = n.elements_;
+        std::transform(u.cbegin(), u.cend(), v.cbegin(), u.begin(), std::minus<float>());
+
+        return Matrix{ m.rows_, m.cols_, u };
+    }
+
+    friend Matrix operator-(const Matrix& m, float value) {
+        Vector v = m.elements_;
+        std::transform(v.cbegin(), v.cend(),v.begin(), [value](float f){return f - value;});
+
+        return Matrix{ m.rows_, m.cols_, v };
+    }
+
+    Matrix operator-() const {
+        Matrix negativeMat = *this;
+        std::transform(negativeMat.elements_.cbegin(), negativeMat.elements_.cend(), negativeMat.elements_.begin(), std::negate<float>());
+
+        return negativeMat;
+    }
+
+    void alloc(Vector elements)
+    {
+        assert(rows_*cols_ == std::size(elements));
+        elements_ = elements;
     }
 
     auto capacity()
     {
-        return m_elements.capacity();
+        return elements_.capacity();
     }
 
-    Matrix col(Index c)
-    {
-        Matrix dst(m_rows, 1);
-        for (Index i{ 0 }; i < m_rows; ++i)
-        {
-            dst.m_elements.at(i) = m_elements.at(c + i*m_cols);
-        }
-
-        return dst;
-    }
-
-    Matrix cols(Index begin, Index end)
-    {
-        assert(end > begin);
-        Index cols{ end - begin + 1 };
-        Matrix dst(m_rows, cols);
-
-        for (Index i{ 0 }; i < m_rows; ++i)
-        {
-            for (Index j{ 0 }; j < cols; ++j)
-            {
-                Index col_index{ j+begin };
-                dst.m_elements.at(i*cols+j) = m_elements.at(i*m_cols+col_index);
-            }
-        }
-        return dst;
-    }
-
-    void copy(Matrix m)
-    {
-        m_rows = m.m_rows;
-        m_cols = m.m_cols;
-        m_elements.clear();
-        m_elements = m.m_elements;
-    }
-    
     Matrix dot(Matrix m)
     {
-        Matrix dst(m_rows, m.m_cols);
-        assert(m_cols == m.m_rows);
-        Index size{ m_cols };
-        assert(dst.m_rows == m_rows);
-        assert(dst.m_cols == m.m_cols);
+        Matrix dst(rows_, m.cols_);
+        assert(cols_ == m.rows_);
+        Index size{ cols_ };
+        assert(dst.rows_ == rows_);
+        assert(dst.cols_ == m.cols_);
 
-        for (Index i{ 0 }; i < dst.m_rows; ++i) {
-            for (Index j{ 0 }; j < dst.m_cols; ++j) {
-                dst.m_elements.at(i*dst.m_cols+j) = 0.0f;
+        for (Index i{ 0 }; i < dst.rows_; ++i) {
+            for (Index j{ 0 }; j < dst.cols_; ++j) {
+                dst.elements_.at(i*dst.cols_+j) = 0.0f;
                 for (Index k{ 0 }; k < size; ++k) {
-                    dst.m_elements.at(i*dst.m_cols+j) += m_elements.at(i*m_cols+k) * m.m_elements.at(k*m.m_cols+j);
+                    dst.elements_.at(i*dst.cols_+j) += elements_.at(i*cols_+k) * m.elements_.at(k*m.cols_+j);
                 }
             }
         }
@@ -102,93 +131,61 @@ public:
 
     void fill(float f)
     {
-        m_elements = Vector(m_rows*m_cols, f);
+        elements_ = Vector(rows_*cols_, f);
+    }
+
+    Matrix col(Index c)
+    {
+        Matrix dst(rows_, 1);
+        for (Index i{ 0 }; i < rows_; ++i)
+        {
+            dst.elements_.at(i) = elements_.at(c + i*cols_);
+        }
+
+        return dst;
     }
 
     Index getCols()
     {
-        return m_cols;
+        return cols_;
     }
 
     Index getRows()
     {
-        return m_rows;
-    }
-    
-    void print(std::string_view name)
-    {
-        std::cout << name << " = [\n";
-        for (Index i{ 0 }; i < m_rows; ++i) {
-            for (Index j{ 0 }; j < m_cols; ++j) {
-                std::cout << "  " << m_elements.at(i*m_cols+j) << " ";
-            }
-            std::cout << '\n';
-        }
-        std::cout << "]\n";
+        return rows_;
     }
 
     void rand(float low, float high)
     {
-        m_elements.clear();
-        for (Index i{ 0 }; i < m_rows*m_cols; ++i)
-            m_elements.push_back(rand_float()*(high-low)+low);
+        std::mt19937 mt{ std::random_device{}() };
+        std::uniform_real_distribution rand(0.0, 1.0);
+
+        for (Index i{ 0 }; i < rows_*cols_; ++i)
+            elements_[i] = (rand(mt)*(high-low)+low);
     }
 
     Matrix row(Index r)
     {
-        Matrix dst(1, m_cols);
-        Index begin_offset{ r*m_cols };
-        Index end_offset{ (r+1)*m_cols };
-        Vector slice(m_elements.begin()+static_cast<long>(begin_offset), m_elements.begin()+static_cast<long>(end_offset));
+        Matrix dst(1, cols_);
+        Index begin_offset{ r*cols_ };
+        Index end_offset{ (r+1)*cols_ };
+        Vector slice(elements_.begin()+static_cast<long>(begin_offset), elements_.begin()+static_cast<long>(end_offset));
         dst.alloc(slice);
 
         return dst;
     }
 
-    void set(Index i, float f)
+    void sigmoid()
     {
-        m_elements.at(i) = f;
-    }
-
-    void sig()
-    {
-        std::for_each(m_elements.begin(), m_elements.end(), [](float &f){ f = sigmoidf(f); });
+        std::for_each(elements_.begin(), elements_.end(), [](float &f){ f = 1.0f / (1.0f + std::exp(-f)); });
     }
 
     auto size()
     {
-        return m_elements.size();
-    }
-
-    void sub_at(Index i, float f)
-    {
-        m_elements.at(i) -= f;
-    }
-
-    void sum(Matrix m)
-    {
-        assert(m_rows == m.m_rows);
-        assert(m_cols == m.m_cols);
-        std::transform(m_elements.begin(), m_elements.end(), m.m_elements.begin(), m_elements.begin(), std::plus<float>());
-    }
-
-    void sum_at(Index i, float f)
-    {
-        m_elements.at(i) += f;
+        return elements_.size();
     }
 };
 
-float rand_float()
-{
-    std::mt19937 mt{ std::random_device{}() };
-    std::uniform_real_distribution rand(0.0, 1.0);
-
-    return rand(mt);
-}
-
-float sigmoidf(float f)
-{
-    return 1.0f / (1.0f + std::expf(-f));
 }
 
 #endif
